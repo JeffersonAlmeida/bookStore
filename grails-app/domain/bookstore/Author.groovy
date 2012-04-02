a2d1348572629cae366cd8b1ec55bef9f0c61411
jefferson <jefferson.almeida.comp@gmail.com>
2012-04-02 20:30:42 -0300

    classes criadas

---
+++ grails-app/domain/bookstore/Author.groovy
@@ -0,0 +1,11 @@
+package bookstore
+
+class Author { 
+   
+    String name
+    static constraints = {
+        name(blank:false)
+    }   
+    static hasMany = [books: Book]
+    
+}
---
+++ grails-app/domain/bookstore/Book.groovy
@@ -0,0 +1,11 @@
+package bookstore
+
+class Book {
+    
+    String title
+    static constraints = {
+        title(blank:false)
+    }
+    
+    static belongsTo = [author: Author]
+}

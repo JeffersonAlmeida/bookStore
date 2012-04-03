b84f73a3ae26ef021e588f7ffbcc0440840ddfa9
jefferson <jefferson.almeida.comp@gmail.com>
2012-04-02 21:08:24 -0300

    Predata in BootStrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,13 +1,11 @@
 import bookstore.*
 class BootStrap {
 
-    def init = { servletContext ->
-        
+    def init = { servletContext ->        
         if(!Book.count()){
             def author = new Author(name: "Dierk konig").save()
-            def book = new Book(title:"Groovy in Action").save()
-        }
-       
+            def authorJ = new Author(name: "Jefferson Almeida").save()
+        }      
     }
     def destroy = {
     }
